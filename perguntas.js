e898a44f59c388631900face1ab9e4a1bd150f7e
Felipe-borges-santos <137516689+Felipe-borges-santos@users.noreply.github.com>
2024-10-08 08:52:43 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+    'Geografia',
+    'Qual o maior país do mundo?',
+    'O maior país do mundo é a Rússia.'
+)
+
+criaCartao(
+    'Cinema',
+    'Qual é o nome do protagonista do filme Kung Fu Panda?',
+    'O nome do protagonista é Po.'
+)
+
+criaCartao(
+    'História',
+    'Quantos anos tem a Muralha da China?',
+    'A Muralha da China tem 2.243 anos.'
+)
